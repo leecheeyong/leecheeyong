4a7ecb4821d63b519eee3f6a4a21c265a7744957
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-12-14 12:49:30 +0800

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -11,5 +11,5 @@ var edited;
 const status = (await axios.get(`https://garden.is-a.dev/v2/discordstatus/785783071244025867`).catch(e => console.log(e))).data;
 const activity = (await axios.get(`https://garden.is-a.dev/v2/discordactivity/785783071244025867`).catch(e => console.log(e))).data;
 const finalCode = eval(`function edit() { \n${codeBlock}\n readMe.status = "${status.message}"; readMe.activity = "${activity.message}"; return readMe }; edit()`);
-fs.writeFileSync('./README.md', `${readMe.replace(codeBlock, `\n${js_beautify(`const readMe = ${finalCode}`, { indent_size: 2, space_in_empty_paren: true })}\n`)}`);
+fs.writeFileSync('./README.md', `${readMe.replace(codeBlock, `\n${js_beautify(`const readMe = ${JSON.stringify(finalCode)}`, { indent_size: 2, space_in_empty_paren: true })}\n`)}`);
 })();
