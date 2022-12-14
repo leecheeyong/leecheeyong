1db387a628fb9d20d281b876b0366e03b52f3b53
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-12-14 12:46:17 +0800

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const substrings = require('substrings');
 const fs = require('fs');
+const js_beautify = require("js-beautify").js;
 
 const readMe = fs.readFileSync('./README.md', 'utf-8');
 const codeBlock = substrings.getOne('```js', '```', readMe);
@@ -10,5 +11,5 @@ var edited;
 const status = (await axios.get(`https://garden.is-a.dev/v2/discordstatus/785783071244025867`).catch(e => console.log(e))).data;
 const activity = (await axios.get(`https://garden.is-a.dev/v2/discordactivity/785783071244025867`).catch(e => console.log(e))).data;
 const finalCode = eval(`function edit() { \n${codeBlock}\n readMe.status = "${status.message}"; readMe.activity = "${activity.message}"; return readMe }; edit()`);
-fs.writeFileSync('./README.md', `${readMe.replace(codeBlock, `\nconst readMe = ${JSON.stringify(finalCode)}`)}`);
+fs.writeFileSync('./README.md', `${readMe.replace(codeBlock, `\n${js_beautify(`const readMe = ${finalCode}`, { indent_size: 2, space_in_empty_paren: true })}\n`)}`);
 })();
