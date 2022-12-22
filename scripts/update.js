ce1ca7bb3fcc7fb4a25ff8facc1f8abaca9391f3
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-12-22 17:21:16 +0800

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -21,6 +21,8 @@ const { stargazers_count } = (await axios.get(`https://api.github.com/repos/leec
 const finalCode = eval(`function edit() { \n${codeBlock}\n readMe.status = "${status.message} - ${activity.message}"; readMe.location = "${location}"; readMe.stats = () => { return "${followers} followers with ${stargazers_count} stars on this repository, ${dayjs().tz("Asia/Taipei").format("DD/MM/YY")}" };  return readMe }; edit()`);
 function objify(obj) { return Object.entries(obj).reduce((a, e) => {if (typeof e[1] != "function") {a += `${e[0]}: "${e[1]}", `;} return a;}, "`{").slice(1, -2) + "}";}
 function str(obj) {return Object.entries(obj)  .reduce((a, e) => {if(Array.isArray(e[1])) { a += `${e[0]}: ${JSON.stringify(e[1])}, `;}else  if(typeof e[1] == "object") {a += `${e[0]}: ${objify(e[1])}, `; }  else if (typeof e[1] == "function") {a += `${e[0]}: ${e[1].toString()}, `;}else {a += `${e[0]}: "${e[1]}", `;}return a;}, "`{").slice(1, -2) + "}";}
+if(status?.message && activity?.message) {
 fs.writeFileSync('./README.md', `${readMe.replace(codeBlock, `\n${js_beautify(`const readMe = ${str(finalCode)}`, { indent_size: 2, space_in_empty_paren: true })}\n`)}`);
-if(status?.message && activity?.message) fs.writeFileSync('./profile/discord.txt', `${status.message} - ${activity.message}`)
+fs.writeFileSync('./profile/discord.txt', `${status.message} - ${activity.message}`)
+}else { console.log("profile not updated") }
 })();
