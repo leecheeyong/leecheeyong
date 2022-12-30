27d6095c6a7a37e2fd2da758ef37d924e666c914
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-12-30 11:16:32 +0800

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -8,7 +8,7 @@ const js_beautify = require("js-beautify").js;
 
 const readMe = fs.readFileSync('./README.md', 'utf-8');
 const codeBlock = substrings.getOne('```js', '```', readMe);
-const code = substrings.getOne('const readMe =', ';', codeBlock);
+const code = JSON.parse(substrings.getOne('const readMe =', ';', codeBlock));
 
 dayjs.extend(timezone);
 dayjs.extend(utc);
@@ -24,7 +24,7 @@ const { stargazers_count } = (await axios.get(`https://api.github.com/repos/leec
   
 console.log(`${status.message} - ${activity.message}` != `${readMe.status}`, `${status.message} - ${activity.message}`, `${readMe.status}`);
   
-const finalCode = eval(`function edit() { \n${codeBlock}\n readMe.status = "${status.message} - ${activity.message}"; readMe.lastSeen = "${`${status.message} - ${activity.message}` != `${readMe.status}` ? `${dayjs().tz("Asia/Taipei").format("HH:mm a")}` : `${readMe.lastSeen}`}"; readMe.location = "${location}"; readMe.stats = () => { return "${followers} followers with ${stargazers_count} stars on this repository, ${dayjs().tz("Asia/Taipei").format("DD/MM/YY")}" };  return readMe }; edit()`);
+const finalCode = eval(`function edit() { \n${codeBlock}\n readMe.status = "${status.message} - ${activity.message}"; readMe.lastSeen = "${`${status.message} - ${activity.message}` != `${code.status}` ? `${dayjs().tz("Asia/Taipei").format("HH:mm a")}` : `${code.lastSeen}`}"; readMe.location = "${location}"; readMe.stats = () => { return "${followers} followers with ${stargazers_count} stars on this repository, ${dayjs().tz("Asia/Taipei").format("DD/MM/YY")}" };  return readMe }; edit()`);
 function objify(obj) { return Object.entries(obj).reduce((a, e) => {if (typeof e[1] != "function") {a += `${e[0]}: "${e[1]}", `;} return a;}, "`{").slice(1, -2) + "}";}
 function str(obj) {return Object.entries(obj)  .reduce((a, e) => {if(Array.isArray(e[1])) { a += `${e[0]}: ${JSON.stringify(e[1])}, `;}else  if(typeof e[1] == "object") {a += `${e[0]}: ${objify(e[1])}, `; }  else if (typeof e[1] == "function") {a += `${e[0]}: ${e[1].toString()}, `;}else {a += `${e[0]}: "${e[1]}", `;}return a;}, "`{").slice(1, -2) + "}";}
 if(status?.message && activity?.message && (status?.message != "Not Found" && activity?.message != "Not Found")) {
