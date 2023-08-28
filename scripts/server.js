7588468dfe7bd92c77afa24ad84e4bcd4c8dec9e
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2023-08-29 01:06:24 +0800

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const spotify = require('spotify-url-info')(fetch);
 const app = express();
 
+
+app.get("/", (req,res) => res.send("Hello World"))
+
 app.get("/artist/:id", async (req,res) => {
   res.send(spotify.getData(req.params.id);
 })
